131188eed155ef231fd894f6a5f6f5f7acb70df4
Gustavo Santiago <santiago.gustavo.mail@gmail.com>
2021-10-03 22:29:32 -0300

    fix(node): adjust heroku dynamic port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const validUrl = require('valid-url');
 
 const app = express();
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const parseUrl = (url) => {
   url = decodeURIComponent(url);
