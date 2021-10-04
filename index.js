e06a15642fef6a5a82202c5a5612de2f81b514b5
Gustavo Santiago <santiago.gustavo.mail@gmail.com>
2021-10-03 22:23:18 -0300

    refactor(puppeteer): update request response, build with valid-url

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const validUrl = require('valid-url');
 
 const app = express();
 const port = process.env.SERVER_PORT || 5000;
 
+const parseUrl = (url) => {
+  url = decodeURIComponent(url);
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = 'http://' + url;
+  }
+
+  return url;
+};
+
 app.get('/puppet', async (req, res) => {
-  try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-    await page.goto('http://google.com');
-    const image = await page.screenshot({ fullPage: true });
-    await browser.close();
-    res.set('Content-Type', 'image/png');
-    res.send(image);
-  } catch (error) {
-    console.log(error);
+  const urlToScreenshot = parseUrl(req.query.url);
+
+  if (validUrl.isWebUri(urlToScreenshot)) {
+    console.log('Screenshotting: ' + urlToScreenshot);
+    (async () => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+
+      const page = await browser.newPage();
+      await page.goto(urlToScreenshot);
+      await page.screenshot().then(function (buffer) {
+        res.setHeader(
+          'Content-Disposition',
+          'attachment;filename="' + urlToScreenshot + '.png"'
+        );
+        res.setHeader('Content-Type', 'image/png');
+        res.send(buffer);
+      });
+
+      await browser.close();
+    })();
+  } else {
+    res.send('Invalid url: ' + urlToScreenshot);
   }
 });
 
